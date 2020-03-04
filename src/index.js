59ff8ec7a16bdfac92fa410d5e70f7afab9ff743
Julia Zaripova <zaripovaur@gmail.com>
2020-03-04 23:18:34 +0400

    add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let dic = {};
+	let arr = str.split('');
+	let arrStr = [];
+
+	for (let i of bracketsConfig) { 
+        if (i[0] == i[1]) {
+            arrStr.push(i[0])
+        } else {
+              dic[i[0]] = i[1];
+      }
 }
+
+    let stack = [];
+    for (let i of arr) {
+        if (dic.hasOwnProperty(i)) {
+            stack.push(i);
+        } else if (dic[stack[stack.length - 1]] == i) {
+                stack.pop();
+        } else if (arrStr.includes(i) && i !== stack[stack.length - 1]){
+          stack.push(i); 
+        } else if (arrStr.includes(i) && i == stack[stack.length - 1]) {
+          stack.pop();
+        }
+
+	}
+	
+
+	return (stack.length == 0) ? true : false;
+	}
