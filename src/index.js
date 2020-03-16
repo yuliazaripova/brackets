55d6f129db70b7c03673550a60e62460e781d664
Julia Zaripova <zaripovaur@gmail.com>
2020-03-16 12:33:20 +0400

    fix brackets

--- src/index.js
+++ src/index.js
@@ -3,28 +3,21 @@ module.exports = function check(str, bracketsConfig) {
 	let arr = str.split('');
 	let arrStr = [];
 
-	for (let i of bracketsConfig) { 
-        if (i[0] == i[1]) {
-            arrStr.push(i[0])
-        } else {
-              dic[i[0]] = i[1];
-      }
-}
+	for (let i of bracketsConfig) {
+		dic[i[0]] = i[1];
+    }
 
     let stack = [];
     for (let i of arr) {
-        if (dic.hasOwnProperty(i)) {
+        if (dic[stack[stack.length-1]] != i) {
             stack.push(i);
-        } else if (dic[stack[stack.length - 1]] == i) {
+        } else  {
                 stack.pop();
-        } else if (arrStr.includes(i) && i !== stack[stack.length - 1]){
-          stack.push(i); 
-        } else if (arrStr.includes(i) && i == stack[stack.length - 1]) {
-          stack.pop();
-        }
+            }
 
 	}
-	
-
 	return (stack.length == 0) ? true : false;
 	}
+
+
+	
